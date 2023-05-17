501305f9853a09e77bc7b706e550e73171c4acc8
imani.devs <imaniroberts.dev@gmail.com>
2023-05-17 17:23:27 -0400

    created map

--- views/Index.jsx
+++ views/Index.jsx
@@ -3,16 +3,22 @@ const React = require("react");
 class Index extends React.Component {
   render() {
     const { log } = this.props;
-    console.log(log)
+    // console.log(log);
     return (
       <div>
-        <a href={`/logs/${log._id}/edit`}>Edit</a>
-        <form
-          action={`/logs/${log._id}?_method=DELETE`}
-          method="POST"
-        >
+        <ul>
+          {log.map((logEntry) => {
+            return (
+              <li>
+                <h2>{logEntry.title}</h2>
+              </li>
+            );
+          })}
+        </ul>
+        {/* <a href={`/logs/${log._id}/edit`}>Edit</a> */}
+        {/* <form action={`/logs/${log._id}?_method=DELETE`} method="POST">
           <input type="submit" value="DELETE" />
-        </form>
+        </form> */}
       </div>
     );
   }
