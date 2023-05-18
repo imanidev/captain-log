1fb825c354f82f3455921ec89f6b2a084ce39d49
imani.devs <imaniroberts.dev@gmail.com>
2023-05-17 22:04:20 -0400

    final push

--- views/New.jsx
+++ views/New.jsx
@@ -1,38 +1,3 @@
-// const React = require("react");
-
-// class New extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <form action="/logs" method="POST">
-//           <label for="title">Title:</label>
-//           <br />
-//           <input type="text" id="title" name="title" />
-//           <br />
-
-//           <label for="entry">Entry:</label>
-//           <br />
-//           <textarea id="entry" name="entry"></textarea>
-//           <br />
-
-//           <label for="shipIsBroken">Ship is Broken:</label>
-//           <br />
-//           <input
-//             type="checkbox"
-//             id="shipIsBroken"
-//             name="shipIsBroken"
-//             value="true"
-//           />
-//           <br />
-//           <input type="submit">Create Log</input>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-
-// module.exports = New;
-
 const React = require("react");
 
 class New extends React.Component {
--- views/Show.jsx
+++ views/Show.jsx
@@ -1,3 +1,4 @@
+
 const React = require("react");
 
 class Show extends React.Component {
@@ -5,19 +6,26 @@ class Show extends React.Component {
     const { log } = this.props;
     return (
       <div>
-        <h1>Show Page</h1>
-        <p>{log.title}</p>
-        <p>{log.entry}</p>
-        <p>{log.shipIsBroken}</p>
-        <p>{log.timestamps}</p>
+        <h1>Entry Page</h1>
+        <p>Title: {log.title}</p>
+        <p>Entry: {log.entry}</p>
+        <p>
+          <b>Created:</b> {new Date(log.createdAt).toLocaleString()}
+        </p>
+        <p>
+          <b> Last Updated:</b> {new Date(log.updatedAt).toLocaleString()}
+        </p>
+        <p>
+          <b>Is Ship Broken?</b> - {log.shipIsBroken ? "Yes" : "No"}
+        </p>
         <a href={`/logs/${log._id}/edit`}>Edit</a>
-        
-        <form>
+
+        <form action={`/logs/${log._id}?_method=DELETE`} method="POST">
           <input type="submit" value="DELETE" />
         </form>
         <br />
         <button>
-          <a href="/logs/new">Create New Log</a>
+          <a href="/logs">Go back</a>
         </button>
       </div>
     );
@@ -25,5 +33,4 @@ class Show extends React.Component {
 }
 
 module.exports = Show;
-
 
